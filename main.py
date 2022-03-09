6262cc69482041b64f67e6864f5dead1bd86a293
git <git@mail.com>
2022-03-09 17:22:15 +0200

    48%+?

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ game_over = False
 fox = Actor("fox")
 fox.pos = 100, 100
 
-coin = Actor("fox")
+coin = Actor("coin")
 coin.pos = 200, 200
 
 def draw():
     screen.fill("green")
     fox.draw()
+    coin.draw()
+    screen.draw.text("score: " + str(score), color="black", topleft=(10, 10))
+
+def place_coin():
+    pass
+
+def time_up():
+    pass
+
+def update():
+    pass
 
 pgzrun.go()
