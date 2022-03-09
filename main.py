7a8670aa8d4f21a8485a0d99dfde846f3ad6104d
git <git@mail.com>
2022-03-09 17:01:37 +0200

    bug

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pgzrun
 import pygame
 
+WIDTH = 400
+HEIGHT = 400
+score = 0
+game_over = False
+fox = Actor("fox")
+fox.pos = 100, 100
 
-pgzrun.go()
+coin = Actor("fox")
+coin.pos = 200, 200
+
+def draw():
+    screen.fill("green")
+    fox.draw()
+
+pgzrun.go
