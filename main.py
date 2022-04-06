5323b7e61e8d126e08a9d9520eb57e2075b64692
rl <attias.harel@gmail.com>
2022-04-06 17:58:07 +0200

    the end

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pgzrun
 import pygame
 from random import randint
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 600
+HEIGHT = 600
 score = 0
 game_over = False
 
@@ -17,16 +17,38 @@ def draw():
     screen.fill("green")
     fox.draw()
     coin.draw()
-    screen.draw.text("score: " + str(score), color="black", topleft=(10, 10))
+    screen.draw.text(str(score) + " $", color="black", topleft=(10, 10))
+
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("total: " + str(score), topleft=(HEIGHT/3.3, WIDTH/2.2), fontsize=60)
 
 def place_coin():
     coin.x = randint(20, (WIDTH - 20))
     coin.y = randint(20, (HEIGHT - 20))
 
 def time_up():
-    pass
+    global game_over
+    game_over = True
 
 def update():
-    pass
+    global score
+    if keyboard.left:
+        fox.x = fox.x - 2
+    if keyboard.up:
+        fox.y = fox.y - 2
+    if keyboard.down:
+        fox.y = fox.y + 2
+    if keyboard.right:
+        fox.x = fox.x + 2
+
+    coin_collected = fox.colliderect(coin)
+
+    if coin_collected:
+        score = score + 30
+        place_coin()
+
+clock.schedule(time_up, 10.0)
+place_coin()
 
 pgzrun.go()
